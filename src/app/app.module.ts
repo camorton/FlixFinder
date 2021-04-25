250e424b560434fa86f860ad8c8451e292b5535a
camorton <69657334+camorton@users.noreply.github.com>
2021-04-25 17:06:59 -0400

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { SearchCriteriaComponent } from './components/search-criteria/search-criteria.component';
+import { WatchlistPageComponent } from './components/watchlist-page/watchlist-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MoviesComponent,
+    SearchCriteriaComponent,
+    WatchlistPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
